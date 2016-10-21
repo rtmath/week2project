cd19cca6444de5eba7ded1bdd9f69ebb5393629c
Ryan Mathisen <ryantmathisen@gmail.com>
2016-10-21 10:08:19 -0700

    add Input Validation to check if user has selected all fields. currently validateInput() will only console.log to display PASS or FAILURE. this commit will also add a sum function to total the user input and return a single number

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,29 @@
-var callQueue = function () {
+var callFadeQueue = function () {
   var myQueue = { queue: 'manageFading', duration: 'slow' };
         $({})
             .queue(function (next) {
                 $('.hideRow').fadeOut('slow', next);
             })
             .queue(function (next) {
-                $('#jsShow').fadeIn(2000, next);
+                $('#cssShow').fadeIn(2000, next);
             });
+}
+
+function sum(num1, num2, num3, num4, num5) {
+  return num1 + num2 + num3 + num4 + num5;
+}
+
+function validateInput(answer1, answer2, answer3, answer4, answer5) {
+  if (answer1 === answer1 && answer2 === answer2 && answer3 === answer3 && answer4 === answer4 && answer5 === answer5) {
+    return true;
+  }
+  else {
+    return false;
+  }
+} <!--TODO:Use IsNaN instead of 'a === a' format-->
+
+function calcUserInput (answer1, answer2, answer3, answer4, answer5) {
+  var total = sum(answer1, answer2, answer3, answer4, answer5);
 }
 
 $(document).ready(function() {
@@ -21,7 +38,13 @@ $(document).ready(function() {
     var answer3 = parseInt($('select[name=question3]').val());
     var answer4 = parseInt($('select[name=question4]').val());
     var answer5 = parseInt($('select[name=question5]').val());
-    // callQueue();
-    console.log(answer1, answer2, answer3, answer4, answer5);
+    // callFadeQueue();
+    if (validateInput(answer1, answer2, answer3, answer4, answer5)===true) {
+      console.log('SUCCESS!');
+    } else {
+      console.log('FAILURE!');
+    }
+
+    // console.log(answer1, answer2, answer3, answer4, answer5);
   });
 });
