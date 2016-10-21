a41aadf6cc7fa2e3590e6a683749718581a81154
Ryan Mathisen <ryantmathisen@gmail.com>
2016-10-21 09:28:57 -0700

    add jquery functionality to hide description and questionnaire, then display a result. this commit will also add a JS queue to manage the fading

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,14 @@
+var callQueue = function () {
+  var myQueue = { queue: 'manageFading', duration: 'slow' };
+        $({})
+            .queue(function (next) {
+                $('.hideRow').fadeOut('slow', next);
+            })
+            .queue(function (next) {
+                $('#jsShow').fadeIn(2000, next);
+            });
+}
+
 $(document).ready(function() {
   $("#descriptionButton").click(function() {
     $("#questionnaire").fadeIn(600)
@@ -5,6 +16,7 @@ $(document).ready(function() {
 
   $("#questionnaire").submit(function(event) {
     event.preventDefault();
+    callQueue();
     console.log("Something happened")
   });
 });
