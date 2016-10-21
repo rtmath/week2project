be257cb89907f58d3b387cca43903b2665ed5f14
Ryan Mathisen <ryantmathisen@gmail.com>
2016-10-21 10:19:51 -0700

    implement and combine validateInput(), sum(), evalUserInput(), and callFadeQueue() functions to display a result based on user's inputs

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,11 @@
-var callFadeQueue = function () {
+var callFadeQueue = function (result) {
   var myQueue = { queue: 'manageFading', duration: 'slow' };
         $({})
             .queue(function (next) {
                 $('.hideRow').fadeOut('slow', next);
             })
             .queue(function (next) {
-                $('#cssShow').fadeIn(2000, next);
+                $(result).fadeIn(2000, next);
             });
 }
 
@@ -22,8 +22,16 @@ function validateInput(answer1, answer2, answer3, answer4, answer5) {
   }
 } <!--TODO:Use IsNaN instead of 'a === a' format-->
 
-function calcUserInput (answer1, answer2, answer3, answer4, answer5) {
-  var total = sum(answer1, answer2, answer3, answer4, answer5);
+function evalUserInput (total) {
+  if (total <= 7) {
+    return "#cssShow";
+  }
+  if (total <= 18 && total > 7) {
+    return "#rubyShow";
+  }
+  if (total <= 23 && total > 18) {
+    return "#cSharpShow";
+  }
 }
 
 $(document).ready(function() {
@@ -38,9 +46,12 @@ $(document).ready(function() {
     var answer3 = parseInt($('select[name=question3]').val());
     var answer4 = parseInt($('select[name=question4]').val());
     var answer5 = parseInt($('select[name=question5]').val());
+    var total = sum(answer1, answer2, answer3, answer4, answer5)
     // callFadeQueue();
     if (validateInput(answer1, answer2, answer3, answer4, answer5)===true) {
       console.log('SUCCESS!');
+      console.log(evalUserInput(total));
+      callFadeQueue(evalUserInput(total));
     } else {
       console.log('FAILURE!');
     }
