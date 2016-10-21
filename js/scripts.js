d7b33f930e95e9f95450e261f01a6336c658bd9d
Ryan Mathisen <ryantmathisen@gmail.com>
2016-10-21 09:48:06 -0700

    add jQuery functionality to parseInt() user input from questionnaire

--- js/scripts.js
+++ js/scripts.js
@@ -10,13 +10,18 @@ var callQueue = function () {
 }
 
 $(document).ready(function() {
-  $("#descriptionButton").click(function() {
-    $("#questionnaire").fadeIn(600)
+  $('#descriptionButton').click(function() {
+    $('#questionnaire').fadeIn(600)
   });
 
-  $("#questionnaire").submit(function(event) {
+  $('#questionnaire').submit(function(event) {
     event.preventDefault();
-    callQueue();
-    console.log("Something happened")
+    var answer1 = parseInt($('select[name=question1]').val());
+    var answer2 = parseInt($('select[name=question2]').val());
+    var answer3 = parseInt($('select[name=question3]').val());
+    var answer4 = parseInt($('select[name=question4]').val());
+    var answer5 = parseInt($('select[name=question5]').val());
+    // callQueue();
+    console.log(answer1, answer2, answer3, answer4, answer5);
   });
 });
